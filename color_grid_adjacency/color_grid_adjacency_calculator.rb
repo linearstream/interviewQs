0972f392d5aba4d1efe5f1de6a7de1f941c11279
linearstream <664134+linearstream@users.noreply.github.com>
2019-12-28 17:00:10 -0500

    Cleanup including all lint issues

--- color_grid_adjacency/color_grid_adjacency_calculator.rb
+++ color_grid_adjacency/color_grid_adjacency_calculator.rb
@@ -1,8 +1,9 @@
+# Stores this item's color and links to neighboring GridItems (n,e,s,w)
 class GridItem
   attr_reader :color, :n, :e, :s, :w
 
   def initialize(color)
-    @color=color
+    @color = color
     @n = nil
     @e = nil
     @s = nil
@@ -15,205 +16,259 @@ class GridItem
   end
 
   def neighbors
-    return [n, e, s, w]
+    [n, e, s, w]
   end
 
   def neighbor(direction)
-    return send(:"#{direction}")
+    send(direction)
   end
 
   def neighbor_color(direction)
-    if nb=neighbor(direction)
+    if (nb = neighbor(direction))
       nb.color
+    else
+      nb # nil
     end
-    return nil
   end
 
   def visited?
-    return @visited ? true : false
+    @visited
   end
 
   def visit
-    @visited=true
+    @visited = true
   end
 
   def count_common_neighbors
+    # visit ourselves
     visit
-    return neighbors.inject(1){ |total, direction|
-      if direction && !direction.visited? && direction.color==@color
-        total + direction.count_common_neighbors
+    # count ourselves (1)
+    neighbors.inject(1) do |total, neighbor|
+      if neighbor && neighbor.count?(@color)
+        total + neighbor.count_common_neighbors
       else
         total
       end
-    }
+    end
+  end
+
+  def count?(color)
+    !visited? && color == @color
   end
 
   def to_s
-    return [
-      ["?", neighbor_color("n"), "?"],
-      [neighbor_color("w"), @color, neighbor_color("e")],
-      ["?", neighbor_color("s"), "?"]
-    ]
+    inspect.map { |line| CSV.generate_line(line) }.join("\n")
   end
 
+  # Gives fog-of-war view for current object. Helpful for debugging
   def inspect
-    border = "-"*12
-    return "'" + border + "\n" +
-      to_s.map{ |row|
-        row.join(", ")
-      }.join("\n") + "\n" +
-      border + "'\n"
+    [['?',                neighbor_color(:n), '?'],
+     [neighbor_color(:w), @color,             neighbor_color(:e)],
+     ['?',                neighbor_color(:s), '?']]
   end
 end
 
 OrderedPair = Struct.new(:row, :col)
 
+# Manages the coordinates of a color map
 class CoordMgr
   def initialize
     @coordinates = {}
   end
 
+  # Returns either the cached coordinate pair or creates a new one and returns
   def generate(ord_pair)
-    if !@coordinates.key?(ord_pair)
+    if @coordinates.key?(ord_pair)
+      @coordinates[ord_pair]
+    else
       @coordinates[ord_pair] = Coord.new(ord_pair, self)
     end
-    return @coordinates[ord_pair]
   end
 end
 
+# Class for managing an ordered pair and its relation with neighbors
 class Coord
-  def initialize(ord_pair, coord_mgr=nil)
+  def initialize(ord_pair, coord_mgr = nil)
     @ord_pair = ord_pair
     @coord_mgr = coord_mgr
   end
 
   def row
-    return @ord_pair.row
+    @ord_pair.row
   end
 
   def col
-    return @ord_pair.col
+    @ord_pair.col
   end
 
   def north
-    return row == 0 ? nil : @coord_mgr.generate(OrderedPair.new(row-1, col))
+    row.zero? ? nil : @coord_mgr.generate(OrderedPair.new(row - 1, col))
   end
 
   def east
-    return @coord_mgr.generate(OrderedPair.new(row, col+1))
+    @coord_mgr.generate(OrderedPair.new(row, col + 1))
   end
 
   def south
-    return @coord_mgr.generate(OrderedPair.new(row+1, col))
+    @coord_mgr.generate(OrderedPair.new(row + 1, col))
   end
 
   def west
-    return col == 0 ? nil : @coord_mgr.generate(OrderedPair.new(row, col-1))
+    col.zero? ? nil : @coord_mgr.generate(OrderedPair.new(row, col - 1))
   end
 
   def to_s
-    return "(#{row}, #{col})"
+    "(#{row}, #{col})"
   end
 
   def inspect
-    return to_s
+    to_s
   end
 end
 
 require 'csv'
+# Loads a color map csv file
 class ColorMapLoader
   def self.load(csv_file)
-    return CSV.read(csv_file)
+    CSV.read(csv_file)
+  rescue StandardError => err
+    raise err.class, "Failed to load csv file #{csv_file}: #{err.message}"
   end
 end
 
+# Provides a representation of a 2d array of color strings
 class ColorMap
   def initialize(color_map_data)
     @data = color_map_data
-    @coord_mgr = CoordMgr.new()
+    @coord_mgr = CoordMgr.new
   end
 
-  def each
-    @data.each_with_index{ |row, row_idx|
-      row.each_with_index{ |color, col_idx|
+  def inspect
+    @data.map do |line|
+      CSV.generate_line(line)
+    end
+  end
+
+  def flat_map
+    @data.each_with_index.flat_map do |row, row_idx|
+      row.each_with_index.map do |color, col_idx|
         coord = @coord_mgr.generate(OrderedPair.new(row_idx, col_idx))
         yield coord, color
-      }
-    }
+      end
+    end
   end
 end
 
+# Linked mapping of GridItems that represent a ColorMap
 class ColorGrid
   def self.generate(color_map)
     grid = build_color_grid(color_map)
     connect_grid_linkage(grid)
-    return new(grid)
+    new(grid, color_map)
   end
 
   def self.build_color_grid(color_map)
-    grid = {}
-    color_map.each{ |coord, color|
-      grid[coord] = GridItem.new(color)
-    }
-    return grid
+    Hash[
+      color_map.flat_map do |coord, color|
+        [coord, GridItem.new(color)]
+      end
+    ]
   end
 
   def self.connect_grid_linkage(grid)
-    grid.map{ |coord, grid_item|
+    grid.map do |coord, grid_item|
       grid_item.neighbors = [
         grid[coord.north],
         grid[coord.east],
         grid[coord.south],
         grid[coord.west]
       ]
-    }
+    end
   end
 
-  def initialize( grid )
-    @grid=grid
+  def initialize(grid, color_map)
+    @grid = grid
+    @color_map = color_map
   end
 
-  def traverse_shared_colors()
-    traversed_results = {}
-    @grid.each{ |coords, item|
-      if !item.visited?
-        traversed_results[coords] = {
-          :color => item.color,
-          :count => item.count_common_neighbors
-        }
+  def inspect
+    @color_map.inspect
+  end
+
+  def traverse_shared_colors
+    Hash[
+      unvisited_items.map do |coords, item|
+        [coords,
+         { color: item.color,
+           count: item.count_common_neighbors }]
       end
-    }
-    return traversed_results
+    ]
+  end
+
+  def unvisited_items
+    @grid.reject do |_coords, item|
+      item.visited?
+    end
   end
 end
 
-class ColorMapMaxCalculator
+# Main application runner class.
+# Call the factory method with a csv with a color mapping.
+class ColorMapMaxAdjacentCalculator
+  attr_reader :color_map
+
+  def self.run(csv_file)
+    cmmac = ColorMapMaxAdjacentCalculator.generate(csv_file)
+    shared_max = cmmac.calculate_shared_max
+    ColorMapMaxAdjacentCalculatorReporter.report(
+      csv_file, cmmac.color_map, shared_max
+    )
+  end
+
   def self.generate(color_map_file)
     # Loads the raw data
     color_map_data = ColorMapLoader.load(color_map_file)
     # ColorMap is stateless and unlinked
     color_map = ColorMap.new(color_map_data)
     # ColorGrid is linked and stateful
-    return self.new(ColorGrid.generate(color_map))
+    new(ColorGrid.generate(color_map), color_map)
   end
 
-  def initialize(color_grid)
+  def initialize(color_grid, color_map)
     @color_grid = color_grid
+    @color_map = color_map
   end
 
   def calculate_shared_max
-    return @color_grid.traverse_shared_colors.max_by{ |coord, color_count|
+    @color_grid.traverse_shared_colors.max_by do |_, color_count|
       color_count[:count]
-    }
+    end
+  end
+end
+
+# Returns an array of strings for the report
+class ColorMapMaxAdjacentCalculatorReporter
+  def self.report(csv_file, color_map, shared_max)
+    report_color_map(csv_file, color_map) +
+      [report_shared_max(shared_max)]
   end
 
-  def report_shared_max(max_data)
+  def self.report_color_map(csv_file, color_map)
+    border_edge = '-' * 3
+    border_mid = '-' * csv_file.length
+    [
+      border_edge + csv_file + border_edge,
+      color_map.inspect,
+      border_edge + border_mid + border_edge
+    ]
+  end
+
+  def self.report_shared_max(max_data)
     coord, data = max_data
-    puts "The max number of adjacent cells with the same color contains the " +
+    'The max number of adjacent cells with the same color contains the ' \
       "cell #{coord}, is color #{data[:color]}, and has size #{data[:count]}"
   end
 end
 
-color_map_calc = ColorMapMaxCalculator.generate('colors.csv')
-max_data = color_map_calc.calculate_shared_max
-color_map_calc.report_shared_max(max_data)
+# Load the file and calculate the max color max adjacent color
+puts ColorMapMaxAdjacentCalculator.run('colors.csv')
